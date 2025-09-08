4b63fb0e6f256324b74777d664a6416a6cb38c5f
sonu alam <sonu.alam@rekise.com>
2025-09-08 18:40:00 +0530

    chore: update package.json to add eslint and typescript-eslint, and update dependencies
    feat: add unused function in main.ts for testing purposes

--- src/main.ts
+++ src/main.ts
@@ -44,6 +44,11 @@ v1Router.get("/healthz", handlerReadiness);
 
 app.use("/v1", v1Router);
 
+function unused() {
+  //this is the functin
+  //for testing
+}
+
 app.listen(config.api.port, () => {
   console.log(`Server is running on port: ${config.api.port}`);
 });
